94f126cb7352dcae7ed62cf7794d133b34678d28
weluvenzo <lorenzoimurillo@gmail.com>
2024-05-09 15:19:19 -0700

    Checkpoint 3, maybe the final products

---
+++ controllers/EventController.js
@@ -0,0 +1,81 @@
+const{Event} = require('../models');
+const zipcodes= ['93706', '93710', '93725', '93722', '93705', '93721', '93727', '93701', '93712', '93726', '93711', '93704', '93703', '93612', '93650', '93702','93703','93720','93723', '93728'];
+
+module.exports.viewAll = async function(req, res) {
+    let searchZipcodes = ['All'];
+    for (let i = 0; i < zipcodes.length; i++) {
+        searchZipcodes.push(zipcodes[i]);
+    }
+    let events;
+    let searchZipcode = req.query.zipcode || 'All';
+    let searchRandom = req.query.random || false; //changed
+    if (searchZipcode === 'All') {
+        events = await Event.findAll();
+    } else {
+        events = await Event.findAll({
+            where: {
+                zipcodes: searchZipcode
+            }
+        });
+    }
+    if(events.length > 0 && searchRandom){
+        let randomIndex = getRandomInt(events.length);
+        events = [events[randomIndex]];
+    }
+    res.render('events', {events: events, zipcodes:searchZipcodes, searchZipcode, searchRandom}); //changed
+}
+module.exports.renderEditForm = async function (req, res) {
+    const event = await Event.findByPk(req.params.id);
+    res.render('edit', {event, zipcodes});
+}
+
+module.exports.updateEvent = async function (req,res) {
+    await Event.update(
+        {
+            name: req.body.name,
+            location: req.body.location,
+            date: req.body.date,
+            description: req.body.description,
+            zipcode: req.body.zipcode
+        },
+        {
+            where:
+                {
+                    id: req.params.id
+                }
+        });
+    res.redirect('/events');
+}
+module.exports.deleteEvent = async function (req, res) {
+    await Event.destroy(
+        {
+            where:
+                {
+                    id: req.params.id
+                }
+        });
+    res.redirect('/events');
+}
+
+module.exports.renderAddForm = function (req, res) {
+    const event = {
+        name: "",
+        Location: "",
+        Date: "",
+        Description: "",
+        zipcodes: zipcodes[0],
+    };
+    res.render('add', {event, zipcodes});
+}
+
+module.exports.addEvent = async function (req, res) {
+    await Event.create(
+        {
+            name: req.body.name,
+            location: req.body.location,
+            date: req.body.date,
+            description: req.body.description,
+            zipcode:req.body.zipcode
+        });
+    res.redirect('/events')
+}
---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const EventController = require('../controllers/EventController')
+const companyController = require('../controllers/companyController')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'At Home' });
+});
+router.get('/events', EventController.viewAll);
+router.get('/cashaid', companyController.companyViewAll);
+//router.post('/cashaid', companyController.companyViewAll);
+router.get('/fostering', companyController.fosteringViewAll);
+router.get('/housing', companyController.housingViewAll);
+router.get('/inhome', companyController.inhomeViewAll);
+router.get('/legalaid', companyController.legalViewAll);
+router.get('/nutrition', companyController.nutritionViewAll);
+router.get('/notifications', companyController.renderNotifications);
+router.get('/edit/:id', EventController.renderEditForm);
+router.post('/edit/:id', EventController.updateEvent);
+router.get('/delete/:id', EventController.deleteEvent);
+router.get('/add', EventController.renderAddForm);
+router.post('/add', EventController.addEvent);
+
+module.exports = router;
+
+/*
+
+
+ */
